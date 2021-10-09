2da7f0e82bd4bf357bfe705bed1b2e8e00468e93
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2021-10-09 21:23:48 +0300

    Add files via upload

---
+++ Speech to Text/MainWindow.xaml.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Speech.Synthesis;
+
+namespace Speech_to_Text
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Enum для хранения состояния распознавателя
+        private enum State
+        {
+            Idle = 0,
+            Accepting = 1,
+            Off = 2,
+        }
+
+        private State RecogState = State.Off;
+        private SpeechRecognitionEngine recognizer;
+        private SpeechSynthesizer synthesizer = null;        
+        private int Hypothesized = 0;
+        private int Recognized = 0;
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Инициализация распознавателя и синтезатора
+            InitializeRecognizerSynthesizer();
+
+            // Проверка на доступность девайса
+            if (SelectInputDevice())
+            {
+                LoadDictationGrammar();
+                ButtonStart.IsEnabled = true;
+                ReadAloud("Speech Engine Ready for Input");
+            }
+        }
+
+        /// <summary>
+        /// Инициализация распознавателя и синтезатора вместе с их событиями
+        /// /// </summary>
+        private void InitializeRecognizerSynthesizer()
+        {
+            var selectedRecognizer = (from e in SpeechRecognitionEngine.InstalledRecognizers()
+                                      where e.Culture.Equals(Thread.CurrentThread.CurrentCulture)
+                                      select e).FirstOrDefault();
+            recognizer = new SpeechRecognitionEngine(selectedRecognizer);
+            recognizer.AudioStateChanged+=new EventHandler<AudioStateChangedEventArgs>(recognizer_AudioStateChanged);
+            recognizer.SpeechHypothesized += new EventHandler<SpeechHypothesizedEventArgs>(recognizer_SpeechHypothesized);
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+            synthesizer = new SpeechSynthesizer();
+        }
+
+        /// <summary>
+        /// Выбор устройства ввода, если оно доступно
+        /// </summary>
+        /// <returns></returns>
+        private bool SelectInputDevice()
+        {
+            bool proceedLoading = true;
+            // если ОС выше XP
+            if (IsOscompatible())
+            {
+                try
+                {
+                    recognizer.SetInputToDefaultAudioDevice();
+                }
+                catch
+                {
+                    proceedLoading = false; // нет устройства ввода звука
+                }
+            }
+            // если ОС XP или ниже
+            else
+                ThreadPool.QueueUserWorkItem(InitSpeechRecogniser);
+            return proceedLoading;
+        }
+
+        /// <summary>
+        /// Узнаём, совместима ли ОС.
+        /// </summary>
+        /// <returns>истина, если больше, чем XP, иначе ложь</returns>
+        private bool IsOscompatible()
+        {
+            OperatingSystem osInfo = Environment.OSVersion;
+            if (osInfo.Version > new Version("6.0"))
+                return true;
+            else
+                return false;
+        }
+
+        private void InitSpeechRecogniser(object o)
+        {
+            recognizer.SetInputToDefaultAudioDevice();
+        }
+
+        /// <summary>
+        /// Загрузка грамматик, одна для команд, другая для диктовки
+        /// </summary>
+        private void LoadDictationGrammar()
+        {
+            GrammarBuilder grammarBuilder = new GrammarBuilder();
+            grammarBuilder.Append(new Choices("End Dictate"));
+            Grammar commandGrammar = new Grammar(grammarBuilder);
+            commandGrammar.Name = "main command grammar";
+            recognizer.LoadGrammar(commandGrammar);
+
+            DictationGrammar dictationGrammar = new DictationGrammar();
+            dictationGrammar.Name = "dictation";
+            recognizer.LoadGrammar(dictationGrammar);
+        }
+
+        #region Recognizer events
+        private void recognizer_AudioStateChanged(object sender, AudioStateChangedEventArgs e)
+        {
+            switch (e.AudioState)
+            {
+                case AudioState.Speech:
+                    LabelStatus.Content = "Listening";
+                    break;
+                case AudioState.Silence:
+                    LabelStatus.Content = "Idle";
+                    break;
+                case AudioState.Stopped:
+                    LabelStatus.Content = "Stopped";
+                    break;
+            }
+        }
+
+        private void recognizer_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            Hypothesized++;
+            LabelHypothesized.Content = "Hypothesized: " + Hypothesized.ToString();
+        }
+        private void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Recognized++;
+            LabelRecognized.Content = "Recognized: " + Recognized.ToString();
+
+            if (RecogState == State.Off)
+                return;
+            float accuracy = (float)e.Result.Confidence;
+            string phrase = e.Result.Text;
+            {
+                if (phrase == "End Dictate")
+                {
+                    RecogState = State.Off;
+                    recognizer.RecognizeAsyncStop();
+                    ReadAloud("Dictation Ended");
+                    return;
+                }
+                TextBox1.AppendText(" " + e.Result.Text);
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Приостановить распознавание и озвучить отправленный текст
+        /// </summary>
+        /// <param name="speakText"></param>
+        public void ReadAloud(string speakText)
+        {
+            try
+            {
+                recognizer.RecognizeAsyncCancel();
+                synthesizer.SpeakAsync(speakText);
+            }
+            catch { }
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            switch (RecogState)
+            {
+                case State.Off:
+                    RecogState = State.Accepting;
+                    ButtonStart.Content = "Stop";
+                    recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+                case State.Accepting:
+                    RecogState = State.Off;
+                    ButtonStart.Content = "Start";
+                    recognizer.RecognizeAsyncStop();
+                    break;
+            }
+        }
+    }
+}
---
+++ SpeechRecognitionSample/Window1.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Speech;
+using System.Speech.Recognition;
+using System.Speech.Recognition.SrgsGrammar;
+using System.Xml;
+
+namespace SpeechRecognitionSample
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        protected SpeechRecognizer spRecognizer ;
+
+
+        public Window1()
+        {
+            InitializeComponent();
+
+            InitializeSR();
+            textBox1.Focus();
+        }
+
+        private void InitializeSR()
+        {
+            spRecognizer = new SpeechRecognizer();
+            spRecognizer.Enabled = true; 
+            spRecognizer.SpeechRecognized += 
+                new EventHandler<SpeechRecognizedEventArgs>(spRecognizer_SpeechRecognized);
+        }
+
+        void spRecognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            textBox1.Text = e.Result.Text;
+        }
+
+        private void EnableSRCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (EnableSRCheckBox.IsChecked != true)
+            {
+                //if (spRecognizer != null)
+                //    spRecognizer.Enabled = false; 
+            }
+            else
+            {
+                InitializeSR();                
+            }
+        }
+
+        private void LoadGrammarButton_Click(object sender, RoutedEventArgs e)
+        {
+            SpeechRecognizer spRecognizer = new SpeechRecognizer();
+            Grammar g = CreateGrammarDocument();
+            spRecognizer.LoadGrammar(g);
+            spRecognizer.UnloadGrammar(g);
+            spRecognizer.UnloadAllGrammars();
+        }
+
+        private void LoadGrammarAsyncButton_Click(object sender, RoutedEventArgs e)
+        {
+            SpeechRecognizer spRecognizer = new SpeechRecognizer();
+            Grammar g = CreateGrammarDocument();
+            spRecognizer.LoadGrammarAsync(g); 
+
+        }
+
+        private Grammar CreateGrammarDocument()
+        {
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            // Construct GrammarBuilder here
+            gBuilder.Append(new Choices("Phone", "Email", "Text"));
+            gBuilder.Append("my");
+            gBuilder.Append(new Choices("Mom", "Dad", "Brother", "Sister"));
+
+            // Create a Grammar from a GrammarBuilder
+            Grammar speechGrammar = new Grammar(gBuilder);
+            return speechGrammar;
+        }
+
+        private void CreateSRGS_Click(object sender, RoutedEventArgs e)
+        {
+            BuildDynamicSRGSDocument();
+        }
+
+        private SrgsDocument BuildDynamicSRGSDocument()
+        {
+            // Create SrgsDocument
+            SrgsDocument document = new SrgsDocument();
+
+            // Create Root Rule
+            SrgsRule rootRule = new SrgsRule("MonthsandDays");
+            rootRule.Scope = SrgsRuleScope.Public;
+
+            rootRule.Elements.Add(new SrgsItem("Months and Days Grammar "));
+
+            // Create months
+            SrgsOneOf oneOfMonths = new SrgsOneOf(
+                new SrgsItem("January"),
+                new SrgsItem("February"),
+                new SrgsItem("March"),
+                new SrgsItem("April"),
+                new SrgsItem("May"),
+                new SrgsItem("June"),
+                new SrgsItem("July"),
+                new SrgsItem("August"),
+                new SrgsItem("September"),
+                new SrgsItem("October"),
+                new SrgsItem("November"),
+                new SrgsItem("December")
+                );
+            SrgsRule ruleMonths = new SrgsRule("Months", oneOfMonths);
+            SrgsItem of = new SrgsItem("of");
+            SrgsItem year = new SrgsItem("year");
+            SrgsItem ruleMonthsItem = new SrgsItem(new SrgsRuleRef(ruleMonths), of, year);
+
+            // Create Days
+            SrgsOneOf oneOfDays = new SrgsOneOf(
+                new SrgsItem("Monday"),
+                new SrgsItem("Tuesday"),
+                new SrgsItem("Wednesday"),
+                new SrgsItem("Thursday"),
+                new SrgsItem("Friday"),
+                new SrgsItem("Saturday"),
+                new SrgsItem("Sunday")
+                );
+            SrgsRule ruleDays = new SrgsRule("Days", oneOfDays);
+            SrgsItem week = new SrgsItem("week");
+            SrgsItem ruleDaysItem = new SrgsItem(new SrgsRuleRef(ruleDays), of, week);
+
+            // Add items to root Rule
+            rootRule.Elements.Add(ruleMonthsItem);
+            rootRule.Elements.Add(ruleDaysItem);
+
+
+            // Add all Rules to Document
+            document.Rules.Add(rootRule, ruleMonths, ruleDays);
+          // Add some extra sperate Rules
+            SrgsText textItem = new SrgsText("Start of the Document.");
+            SrgsRule textRule = new SrgsRule("TextItem");
+            textRule.Elements.Add(textItem);
+            document.Rules.Add(textRule);
+
+            SrgsItem stringItem = new SrgsItem("Item as String.");
+            SrgsRule itemRule = new SrgsRule("ItemRule");
+            itemRule.Elements.Add(stringItem);
+            document.Rules.Add(itemRule);
+
+            SrgsItem elementItem = new SrgsItem();
+            SrgsRule elementRule = new SrgsRule("ElementRule");
+            elementRule.Elements.Add(elementItem);
+            document.Rules.Add(elementRule);
+
+            // Set Document Root
+            document.Root = rootRule;
+
+            // Save Created SRGS Document to XML file
+            XmlWriter writer = XmlWriter.Create("DynamicSRGSDocument.Xml");
+            document.WriteSrgs(writer);
+            writer.Close();
+
+            return document;
+        }
+
+        private void LoadSRGS_Click(object sender, RoutedEventArgs e)
+        {
+            SpeechRecognizer spRecognizer = new SpeechRecognizer();
+            spRecognizer.LoadGrammar(new Grammar(BuildDynamicSRGSDocument()));  
+        }
+    }
+}
